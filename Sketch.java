e9383cc05c8938f062fe3acd87c2e245a38750cd
Emma Chan <emma.chan25@ycdsbk12.ca>
2024-02-21 10:18:28 -0500

    FInished Sketch

--- Sketch.java
+++ Sketch.java
@@ -2,12 +2,13 @@ import processing.core.PApplet;
 
 public class Sketch extends PApplet {
 	
-	
-  /**
-   * Called once at the beginning of execution, put your size all in this method
-   */
+/**
+ * This program sketches out a house 
+ * @author: Emma Chan 
+ * 
+ * */	
   public void settings() {
-	// put your size call here
+	//size call 
     size(400, 400);
   }
 
@@ -16,7 +17,7 @@ public class Sketch extends PApplet {
    * values here i.e background, stroke, fill etc.
    */
   public void setup() {
-    background(210, 255, 173);
+    background(138, 181, 237);
   }
 
   /**
@@ -24,13 +25,30 @@ public class Sketch extends PApplet {
    */
   public void draw() {
 	  
-	// sample code, delete this stuff
-    stroke(128);
-    line(150, 25, 270, 350);  
+	// background
+    fill(21, 153, 51);
+    rect(0,250,400,400);
 
-    stroke(255);
-    line(50, 125, 70, 50);  
+  // roof
+    fill(150,58,38);
+    triangle(100, 200, 200, 80, 300, 200);  
+
+  //main part of house
+    fill(255,77,207);
+    rect(100,200,200,200);
+
+  // window 
+    fill (247, 188, 224);
+    rect(130, 240, 70, 70);
+
+  //door 
+    fill (211, 188, 247);
+    rect(230, 310, 60, 100);
+
+  //door knob
+    fill(205, 219, 96);
+    ellipse(250, 360, 10, 10);
+
+
   }
-  
-  // define other methods down here.
 }
